df14ab1996f35e008d925ac474adb6ccf8a39d7c
Dmitry Shchelchkov <dimanonim@gmail.com>
2020-09-26 00:57:47 +0200

    Make it so the bot can be run from the command line.

---
+++ katazza_bot/__main__.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater
+import logging
+from katazza_bot.handlers import init_handlers
+from katazza_bot.config import TELEGRAM_BOT_TOKEN
+
+def run(telegram_bot_token=TELEGRAM_BOT_TOKEN):
+    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    init_handlers(dispatcher)
+
+    updater.start_polling()
+
+if __name__ == '__main__':
+    run()
